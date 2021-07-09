a02e40ccf3cfa8321a2ae4f9de3c38171750b2c0
Musa Muhammad <musaawwaletudaye@gmail.com>
2021-07-09 18:19:09 +0100

    feat: Day1-Ex

---
+++ 01_Introduction/datatypes.js
@@ -0,0 +1,36 @@
+let myName = "AWWAL";//string
+let isMad = true;//boolean
+let companyName;//undefined
+let muslimName = null;//null
+  
+console.log(typeof myName);
+console.log(typeof isMad);
+console.log(typeof companyName);
+console.log(typeof muslimName);
+
+let myOruko, myAgeeee, isComing, isSleeping;
+
+let myGrade = 10,
+  myIq = 100,
+isGoing = true,
+  isRunning = false;
+
+
+let initialName = "Muhammad";
+let lastName = "Musa";
+let maritalStatus = "single and not searching";
+let countrry = "Nigeria";
+let myAgeee= 15;
+
+
+let InitialName = "Muhammad",
+  lasstName = "Musa",
+  maritaalStatus = "single and not searching",
+  country = "Nigeria",
+  myAgee = 15;
+  
+let myAge = 25;
+let yourAge = 30;
+
+console.log("i am " + myAge + " years old ");
+console.log("You are " + yourAge + " years old ");
---
+++ 01_Introduction/variable.js
@@ -0,0 +1,4 @@
+let myName = "AWWAL";//string
+let isMad = false; //boolean
+let companyName;//undefined
+let muslimName = null;//null
